f6ebe58dcaf33fe6aa80cddfd00a7088db9daad4
Huseyin Gundogdu <hgundogdu53@gmail.com>
2019-09-23 22:18:38 -0700

    proptypes added and fixed

--- src/AddFolder/AddFolder.js
+++ src/AddFolder/AddFolder.js
@@ -1,24 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import NotefulForm from '../NotefulForm/NotefulForm'
 import ValidationError from './ValidationError';
-import NotesContext from '../NotesContext';
-import config from '../config';
+import ApiContext from '../ApiContext'
+import config from '../config'
+import PropTypes from 'prop-types'
 
 
 export default class AddFolder extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            name: '',
             folderValid: false,
             validMessage: ''
         }
     }
-
-    static contextType = NotesContext;
-
-    updateFolder(name) {
-        this.setState({ name: name }, () => { this.validateFolder(name) })
+    static defaultProps = {
+        history: {
+            push: () => { }
+        },
     }
+    static contextType = ApiContext;
 
     validateFolder(inputValue) {
         let errorMsg = this.state.validMessage;
@@ -45,44 +46,65 @@ export default class AddFolder extends Component {
 
     }
 
-    addFolderRequest(name, addFolder) {
+    handleSubmit = e => {
+        e.preventDefault()
+        const folder = {
+            name: e.target['folder-name'].value
+        }
         fetch(`${config.API_ENDPOINT}/folders`, {
             method: 'POST',
             headers: {
                 'content-type': 'application/json'
             },
-            body: JSON.stringify({ name: name })
+            body: JSON.stringify(folder),
         })
             .then(res => {
-                if (!res.ok) {
-                    throw new Error('Unable to add folder to database')
-                }
-                return res.json();
+                if (!res.ok)
+                    return res.json().then(e => Promise.reject(e))
+                return res.json()
             })
-            .then(res => addFolder(res))
-            .catch(err => console.log(name, err))
+            .then(folder => {
+                this.context.addFolder(folder)
+                this.props.history.push(`/folder/${folder.id}`)
+            })
+            .catch(error => {
+                console.error({ error })
+            })
     }
 
     render() {
-        console.log(this.props)
-
-        const { addFolder } = this.context
-
-        this.handleSubmit = (event) => {
-            event.preventDefault();
-            console.log(addFolder)
-            this.addFolderRequest(this.state.name, addFolder);
-        }
-
         return (
-            <form onSubmit={(e) => this.handleSubmit(e)}>
-                <label>Add Folder:
-          <input onChange={(e) => this.updateFolder(e.target.value)} type="text" name="addFolder" id="addFolder"></input>
-                </label>
-                <ValidationError hasError={!this.state.folderValid} message={this.state.validMessage} />
-                <button type="submit" disabled={!this.state.folderValid}>Submit</button>
-            </form>
+            <section className='AddFolder'>
+                <h2>Create a folder</h2>
+
+                <NotefulForm onSubmit={this.handleSubmit}>
+                    <div className='field'>
+                        <label htmlFor='folder-name-input'>
+                            Name
+            </label>
+                        <input type='text' id='folder-name-input' name='folder-name' />
+                    </div>
+                    <ValidationError hasError={!this.state.folderValid} message={this.state.validMessage} />
+
+                    <div className='buttons'>
+                        <button type='submit'>
+                            Add folder
+            </button>
+                    </div>
+                </NotefulForm>
+            </section>
         )
     }
-
 }
+
+AddFolder.defaultProps = {
+    history: {
+        push: () => []
+    }
+};
+
+AddFolder.propType = {
+    history: PropTypes.shape({
+        push: PropTypes.func
+    })
+};
--- src/App/App.js
+++ src/App/App.js
@@ -1,19 +1,21 @@
-import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import NoteListNav from '../NoteListNav/NoteListNav';
-import NotePageNav from '../NotePageNav/NotePageNav';
-import NoteListMain from '../NoteListMain/NoteListMain';
-import NotePageMain from '../NotePageMain/NotePageMain';
-import ApiContext from '../ApiContext';
-import config from '../config';
+import React, { Component } from 'react'
+import { Route, Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import NoteListNav from '../NoteListNav/NoteListNav'
+import NotePageNav from '../NotePageNav/NotePageNav'
+import NoteListMain from '../NoteListMain/NoteListMain'
+import NotePageMain from '../NotePageMain/NotePageMain'
+import AddFolder from '../AddFolder/AddFolder'
+import AddNote from '../AddNote/AddNote'
+import ApiContext from '../ApiContext'
+import config from '../config'
 import HandleError from '../HandleError';
-import './App.css';
+import './App.css'
 
 class App extends Component {
     state = {
         notes: [],
-        folders: []
+        folders: [],
     };
 
     componentDidMount() {
@@ -23,87 +25,131 @@ class App extends Component {
         ])
             .then(([notesRes, foldersRes]) => {
                 if (!notesRes.ok)
-                    return notesRes.json().then(e => Promise.reject(e));
+                    return notesRes.json().then(e => Promise.reject(e))
                 if (!foldersRes.ok)
-                    return foldersRes.json().then(e => Promise.reject(e));
+                    return foldersRes.json().then(e => Promise.reject(e))
 
-                return Promise.all([notesRes.json(), foldersRes.json()]);
+                return Promise.all([
+                    notesRes.json(),
+                    foldersRes.json(),
+                ])
             })
             .then(([notes, folders]) => {
-                this.setState({ notes, folders });
+                this.setState({ notes, folders })
             })
             .catch(error => {
-                console.error({ error });
-            });
+                console.error({ error })
+            })
+    }
+
+    handleAddFolder = folder => {
+        this.setState({
+            folders: [
+                ...this.state.folders,
+                folder
+            ]
+        })
+    }
+
+    handleAddNote = note => {
+        this.setState({
+            notes: [
+                ...this.state.notes,
+                note
+            ]
+        })
     }
 
     handleDeleteNote = noteId => {
         this.setState({
             notes: this.state.notes.filter(note => note.id !== noteId)
-        });
-    };
+        })
+    }
 
     renderNavRoutes() {
         return (
-            <HandleError>
-                <>
-                    {['/', '/folder/:folderId'].map(path => (
-                        <Route
-                            exact
-                            key={path}
-                            path={path}
-                            component={NoteListNav}
-                        />
-                    ))}
-                    <Route path="/note/:noteId" component={NotePageNav} />
-                    <Route path="/add-folder" component={NotePageNav} />
-                    <Route path="/add-note" component={NotePageNav} />
-                </>
-            </HandleError>
-        );
+            <>
+                {['/', '/folder/:folderId'].map(path =>
+                    <Route
+                        exact
+                        key={path}
+                        path={path}
+                        component={NoteListNav}
+                    />
+                )}
+                <Route
+                    path='/note/:noteId'
+                    component={NotePageNav}
+                />
+                <Route
+                    path='/add-folder'
+                    component={NotePageNav}
+                />
+                <Route
+                    path='/add-note'
+                    component={NotePageNav}
+                />
+            </>
+        )
     }
 
     renderMainRoutes() {
         return (
-            <HandleError>
-                <>
-                    {['/', '/folder/:folderId'].map(path => (
-                        <Route
-                            exact
-                            key={path}
-                            path={path}
-                            component={NoteListMain}
-                        />
-                    ))}
-                    <Route path="/note/:noteId" component={NotePageMain} />
-                </>
-            </HandleError>
-        );
+            <>
+                {['/', '/folder/:folderId'].map(path =>
+                    <Route
+                        exact
+                        key={path}
+                        path={path}
+                        component={NoteListMain}
+                    />
+                )}
+                <Route
+                    path='/note/:noteId'
+                    component={NotePageMain}
+                />
+                <Route
+                    path='/add-folder'
+                    component={AddFolder}
+                />
+                <Route
+                    path='/add-note'
+                    component={AddNote}
+                />
+            </>
+        )
     }
 
     render() {
         const value = {
             notes: this.state.notes,
             folders: this.state.folders,
-            deleteNote: this.handleDeleteNote
-        };
+            addFolder: this.handleAddFolder,
+            addNote: this.handleAddNote,
+            deleteNote: this.handleDeleteNote,
+        }
         return (
             <HandleError>
                 <ApiContext.Provider value={value}>
-                    <div className="App">
-                        <nav className="App__nav">{this.renderNavRoutes()}</nav>
-                        <header className="App__header">
+                    <div className='App'>
+                        <nav className='App__nav'>
+                            {this.renderNavRoutes()}
+                        </nav>
+                        <header className='App__header'>
                             <h1>
-                                <Link to="/">Noteful</Link>{' '}
-                                <FontAwesomeIcon icon="check-double" />
+                                <Link to='/'>Noteful</Link>
+                                {' '}
+                                <FontAwesomeIcon icon='check-double' />
                             </h1>
                         </header>
-                        <main className="App__main">{this.renderMainRoutes()}</main>
+                        <main className='App__main'>
+                            {this.renderMainRoutes()}
+                        </main>
                     </div>
                 </ApiContext.Provider>
             </HandleError>
-        );
+        )
     }
 }
 
-export default App;
+export default App
